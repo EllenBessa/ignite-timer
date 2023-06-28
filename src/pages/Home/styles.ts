abb47a6e4e9f65677489dbbcae4f3c19332fbcb9
EllenBessa <ellenbessa96@gmail.com>
2023-06-28 10:12:17 -0300

    feat: :sparkles: Funcionalidades da aplicação - Interromper ciclo

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -90,11 +90,13 @@ export const Separator = styled.div`
   justify-content: center;
 `;
 
-export const StartCountdownButton = styled.button`
+export const BaseCountdownButton = styled.button`
   width: 100%;
   border: 0;
   padding: 1rem;
   border-radius: 8px;
+
+  color: ${(props) => props.theme["gray-100"]};
 
   display: flex;
   align-items: center;
@@ -102,8 +104,6 @@ export const StartCountdownButton = styled.button`
   gap: 0.5rem;
 
   font-weight: 700;
-  background: ${(props) => props.theme["green-500"]};
-  color: ${(props) => props.theme["gray-100"]};
 
   cursor: pointer;
 
@@ -111,8 +111,20 @@ export const StartCountdownButton = styled.button`
     opacity: 0.7;
     cursor: not-allowed;
   }
+`;
+
+export const StartCountdownButton = styled(BaseCountdownButton)`
+  background: ${(props) => props.theme["green-500"]};
 
   &:not(:disabled):hover {
     background: ${(props) => props.theme["green-700"]};
+  }
+`;
+
+export const StopCountdownButton = styled(BaseCountdownButton)`
+  background: ${(props) => props.theme["red-500"]};
+
+  &:not(:disabled):hover {
+    background: ${(props) => props.theme["red-700"]};
   }
 `;
