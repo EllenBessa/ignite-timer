3080fcb73ba9170b41a9dc392e235784f680ce25
EllenBessa <ellenbessa96@gmail.com>
2023-06-29 12:28:03 -0300

    feat: :sparkles: Contexto no React - Convertendo para contexto

--- src/pages/Home/components/Countdown/index.tsx
+++ src/pages/Home/components/Countdown/index.tsx
@@ -1,13 +1,12 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { differenceInSeconds } from "date-fns";
 
 import { CountdownContainer, Separator } from "./styles";
+import { CyclesContext } from "../..";
 
-interface CountdownProps{
-  activeCycle: any
-}
-
-export function Countdown({activeCycle}:CountdownProps) {
+export function Countdown() {
+  const { activeCycle, activeCycleId, markCurrentCycleAsFinished } =
+    useContext(CyclesContext);
   const [amountSecondsPassed, setAmountSecondsPassed] = useState(0);
 
   const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0;
@@ -23,15 +22,7 @@ export function Countdown({activeCycle}:CountdownProps) {
         );
 
         if (secondsDifference >= totalSeconds) {
-          setCycles((state) =>
-            state.map((cycle) => {
-              if (cycle.id === activeCycleId) {
-                return { ...cycle, finishedDate: new Date() };
-              } else {
-                return cycle;
-              }
-            })
-          );
+          markCurrentCycleAsFinished();
 
           setAmountSecondsPassed(totalSeconds);
           clearInterval(interval);
@@ -44,18 +35,31 @@ export function Countdown({activeCycle}:CountdownProps) {
     return () => {
       clearInterval(interval);
     };
-  }, [activeCycle, totalSeconds, activeCycleId]);
+  }, [activeCycle, totalSeconds, activeCycleId, markCurrentCycleAsFinished]);
+
+  const currentSeconds = activeCycle ? totalSeconds - amountSecondsPassed : 0;
+
+  const minutesAmount = Math.floor(currentSeconds / 60);
+  const secondsAmount = currentSeconds % 60;
+
+  const minutes = String(minutesAmount).padStart(2, "0");
+  const seconds = String(secondsAmount).padStart(2, "0");
+
+  useEffect(() => {
+    if (activeCycle) {
+      document.title = `${minutes}:${seconds}`;
+    }
+  }, [minutes, seconds, activeCycle]);
 
   return (
-      <CountdownContainer>
-        <span>{minutes[0]}</span>
-        <span>{minutes[1]}</span>
+    <CountdownContainer>
+      <span>{minutes[0]}</span>
+      <span>{minutes[1]}</span>
 
-        <Separator>:</Separator>
+      <Separator>:</Separator>
 
-        <span>{seconds[0]}</span>
-        <span>{seconds[1]}</span>
-      </CountdownContainer>
-      ;
+      <span>{seconds[0]}</span>
+      <span>{seconds[1]}</span>
+    </CountdownContainer>
   );
 }
