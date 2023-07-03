0a687d60112405284448c7f240b7cc55b9dfd870
Ellen Bessa <Ellen Bessa>
2023-07-03 11:56:06 -0300

    feat: :sparkles: Contexto no React - Contexto no formulário

--- src/pages/Home/components/NewCycleForm/index.tsx
+++ src/pages/Home/components/NewCycleForm/index.tsx
@@ -1,27 +1,12 @@
-import * as zod from "@hookform/resolvers/zod";
+import { useFormContext } from "react-hook-form";
+import { useContext } from "react";
+import { CyclesContext } from "../..";
 
 import { FormContainer, TaskInput, MinutesAmountInput } from "./styles";
 
-import { useForm } from "react-hook-form";
-
-const newCycleFormValidationSchema = zod.object({
-  task: zod.string().min(1, "Informe a tarefa"),
-  minutesAmount: zod
-    .number()
-    .min(5, "O ciclo precisa ser de no mínimo 5 minutos")
-    .max(60, "O ciclo precisa ser de no máximo 60 minutos")
-});
-
-type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>;
-
 export function NewCycleForm() {
-  const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
-    resolver: zod.zodResolver(newCycleFormValidationSchema),
-    defaultValues: {
-      task: "",
-      minutesAmount: 0
-    }
-  });
+  const { activeCycle } = useContext(CyclesContext);
+  const { register } = useFormContext();
 
   return (
     <FormContainer>
